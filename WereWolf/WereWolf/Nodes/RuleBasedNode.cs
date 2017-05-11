d1e1aa44450fcb5a9149b5840a2442f934aa106c
Marcelo Silva <m.m.silva@accenture.com>
2017-05-12 00:30:20 +0100

    Nodes

---
+++ WereWolf/WereWolf/Nodes/RuleBasedNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WereWolf.Nodes
+{
+    public class RuleBasedNode : PlayerNode
+    {
+        public RuleBasedNode(string playerName, string charName, InformationSet infoSet) : base(playerName, charName, infoSet)
+        {
+        }
+
+        public override int PlayGame(RolloutGame game, int alpha, int beta, int depthLimit, string command)
+        {
+            return -1;
+        }
+    }
+}
